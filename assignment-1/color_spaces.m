250669480f08e38f4c21f0d4623c81e3e88b6cb2
Minh Ngo <nlminhtl@gmail.com>
2016-02-14 22:57:50 +0100

    Refactoring

--- assignment-1/color_spaces.m
+++ assignment-1/color_spaces.m
@@ -1,36 +1,30 @@
 function color_spaces(image_name, color_space)
     img = double(imread(image_name));
+    R = img(:,:,1);
+    G = img(:,:,2);
+    B = img(:,:,3);
     %% Opponent color space
     if strcmp(color_space, 'opponent')
-        o1 = (img(:,:,1) - img(:,:,2)) / sqrt(2.0);
-        o2 = (img(:,:,1) + img(:,:,2) - 2 * img(:,:,3)) / sqrt(6.0);
-        o3 = sum(img, 3) / sqrt(3.0);
-        subplot(3, 3,1);
-        imshow(o1);
-        subplot(3,3,2);
-        imshow(o2);
-        subplot(3,3,3);
-        imshow(o3);
+        o1 = (R - G) / sqrt(2);
+        o2 = (R + G - 2 * B) / sqrt(6);
+        o3 = (R + G + B) / sqrt(3);
+        new_img = cat(3, o1, o2, o3);
     %% Normalized RGB
     elseif strcmp(color_space, 'rgb')
-        norm = double(sum(img, 3));
-        r = img(:,:,1) ./ norm;
-        g = img(:,:,2) ./ norm;
-        b = img(:,:,3) ./ norm;
-        subplot(3, 3,1);
-        imshow(r);
-        subplot(3,3,2);
-        imshow(g);
-        subplot(3,3,3);
-        imshow(b);
+        norm = R + G + B;
+        r = R ./ norm;
+        g = G ./ norm;
+        b = B ./ norm;
+        new_img = cat(3, r, g, b);
+    %% HSV
     elseif strcmp(color_space, 'hsv')
-        hsv = rgb2hsv(img);
-        subplot(3, 3,1);
-        imshow(hsv(:,:,1));
-        subplot(3,3,2);
-        imshow(hsv(:,:,2));
-        subplot(3,3,3);
-        imshow(hsv(:,:,3));
+        new_img = rgb2hsv(img);
     end
+    subplot(3, 3,1);
+    imshow(new_img(:,:,1));
+    subplot(3,3,2);
+    imshow(new_img(:,:,2));
+    subplot(3,3,3);
+    imshow(new_img(:,:,3));
 end
 
