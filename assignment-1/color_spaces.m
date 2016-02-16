0f18c8fcbb460f14913089c2026166dbe171c6e8
Minh Ngo <nlminhtl@gmail.com>
2016-02-16 20:46:07 +0100

    A bit of refactoring

--- assignment-1/color_spaces.m
+++ assignment-1/color_spaces.m
@@ -8,23 +8,31 @@ function color_spaces(image_name, color_space)
         o1 = (R - G) / sqrt(2);
         o2 = (R + G - 2 * B) / sqrt(6);
         o3 = (R + G + B) / sqrt(3);
-        new_img = cat(3, o1, o2, o3);
+        c1 = o1;
+        c2 = o2;
+        c3 = o3;
     %% Normalized RGB
     elseif strcmp(color_space, 'rgb')
         norm = R + G + B;
         r = R ./ norm;
         g = G ./ norm;
         b = B ./ norm;
-        new_img = cat(3, r, g, b);
+        c1 = r;
+        c2 = g;
+        c3 = b;
     %% HSV
     elseif strcmp(color_space, 'hsv')
         new_img = rgb2hsv(img);
+        c1 = new_img(:,:,1);
+        c2 = new_img(:,:,2);
+        c3 = new_img(:,:,3);
     end
-    subplot(3, 3,1);
-    imshow(new_img(:,:,1));
-    subplot(3,3,2);
-    imshow(new_img(:,:,2));
-    subplot(3,3,3);
-    imshow(new_img(:,:,3));
+    %% im2double automatically scales data if it's required
+    subplot(2, 2, 1);
+    imshow(im2double(c1));
+    subplot(2, 2, 2);
+    imshow(im2double(c2));
+    subplot(2, 2, 3);
+    imshow(im2double(c3));
 end
 
