d6f0a59e62475cb885db8653a0ffc6808bca3ca5
Minh Ngo <nlminhtl@gmail.com>
2016-03-02 13:03:46 +0100

    Update tracking

--- assignment-3/tracking.m
+++ assignment-3/tracking.m
@@ -1,44 +1,59 @@
 function tracking()
-    dirName = '/home/ignotus/Development/cv1/assignment-3/person_toy/';
-    listing = dir(fullfile(dirName, '*.jpg'));
+    track_impl('/home/ignotus/Development/cv1/assignment-3/person_toy/', '*.jpg');
+    %%track_impl('/home/ignotus/Development/cv1/assignment-3/pingpong/', '*.jpeg');
+end
+
+function track_impl(dirName, file_pattern)
+    sigma = 3;
+    kernel_length = 11;
+    radius = floor(kernel_length / 2);
+    region_radius = 7;
+    
+    listing = dir(fullfile(dirName, file_pattern));
+    
     files = { listing.name };
     
     first_file_path = fullfile(dirName, files{1});
     img = im2double(imread(first_file_path));
-    [h, w, ~] = size(img);
+    [h, w, d] = size(img);
     
     img = zeros(h, w, length(files));
 
     % stacks images to form a tensor
     for i=1:numel(files)
+        % summing the 3rd axis up to compute intensity
         img(:,:,i) = sum(im2double(imread(fullfile(dirName, files{i}))), 3);
     end
     
-    sigma = 3;
-    kernel_length = 11;
-    radius = floor(kernel_length / 2);
+    rgb = zeros(h, w, d, length(files));
+    % stacks original rgb images
+    for i=1:numel(files)
+        rgb(:,:,:,i) = im2double(imread(fullfile(dirName, files{i})));
+    end
+    
+    
     G = gaussian(sigma, kernel_length);
     Gd = gaussianDer(G, sigma);
-
     
     [~, r, c] = harris(first_file_path);
-    region_radius = 7;
     
     fid = figure;
     writerObj = VideoWriter('out.avi');
-    writerObj.FrameRate = 60;
+    writerObj.FrameRate = 15;
     open(writerObj); 
     
     % for each frame
     for i=1:numel(files)-1
         % Computes derivatives
         Id = zeros(h, w, 3);
+        %% Convolves horizontally
         for x=radius+1:w-radius
             for y=1:h
                 Id(y, x, 1) = sum(Gd' .* img(y, x-radius:x+radius, i));
             end
         end
 
+        %% Convolves vertically
         for x=1:w
             for y=radius+1:h-radius
                 Id(y, x, 2) = sum(Gd .* img(y-radius:y+radius, x, i));
@@ -47,10 +62,10 @@ function tracking()
 
         Id(:,:,3) = img(:,:,i + 1) - img(:,:,i);
         
-        pause(0.1);
+        
         figure(fid);
         
-        imshow(img(:,:, i));
+        imshow(rgb(:,:,:,i));
         hold on;
         
         v = zeros(length(r), 2);
@@ -81,10 +96,11 @@ function tracking()
             v(j,:) = linsolve(A' * A, A' * b);
         end
         
+        %% Updating corner points estimation
         r = int16(double(r) + v(:,1)');
         c = int16(double(c) + v(:,2)');
         
-        plot(r, c, 'go');
+        plot(r, c, 'b^');
         frame = getframe(gcf);
         writeVideo(writerObj, frame);
         hold off
