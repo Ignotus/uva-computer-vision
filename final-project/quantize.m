4b64699c2fa66b8a6f8146599c7ea1316f420a04
Riaan <riaan.zoetmulder@student.uva.nl>
2016-03-24 15:09:53 +0100

    histogram and quantization
    Made a small error in the previous version. now corrected!

--- final-project/quantize.m
+++ final-project/quantize.m
@@ -3,16 +3,19 @@
 
 function [h] = quantize(centroids, datapoints)
 
+% ensure the data is normalized
 normalized_data = normalize(datapoints);
+index =zeros(1,size(normalized_data,2));
 
-index =[];
+% iterate through the data and find the closest point
 for x = 1: size(normalized_data,2)
-    [~, k] = min(vl_alldist2(normalized_data, centroids));
-    [index ; k];
+    [~,k] = min(vl_alldist(normalized_data(:,x), centroids));
+    index(1,x) = k;
     
 end
 
-h = histogram(index, size(centroids,2), 'Normalization', probability);
+% return the histogram
+h = histogram(index, size(centroids,2), 'Normalization', 'probability');
 
 end
 
