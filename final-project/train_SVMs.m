30248ca9ec219b91b0ac948b3eb7e4517aa235c0
Riaan <riaan.zoetmulder@student.uva.nl>
2016-03-24 17:00:07 +0100

    Support vector machine biatch
    support vector machines motherfucker

---
+++ final-project/train_SVMs.m
@@ -0,0 +1,29 @@
+% Support Vector Machine Training function
+
+function [svm1, svm2, svm3, svm4] = train_SVMs(target, data)
+    
+    % Train the first SVM
+    target_1 = target;
+    target_1(target_1 > 1) = 0; 
+    svm1 = svmtrain(target_1, data);
+    
+    % Train the second SVM
+    target_2 = target;
+    target_2(target_2 > 2 || target_2 < 2) = 0;
+    target_2(target_2 == 2) = 1;
+    svm2 = svmtrain(target_2, data);
+    
+    % Train the third SVM
+    target_3 = target;
+    target_3(target_3 > 3 || target_3 < 3) = 0;
+    target_3(target_3 == 3) = 1;
+    svm3 = svmtrain(target_3, data);
+    
+    % Train the fourth SVM
+    target_4 = target;
+    target_4(target_4 < 4) = 0;
+    target_4(target_4 == 4) = 1;
+    svm4 = svmtrain(target_4, data);
+    
+end
+
