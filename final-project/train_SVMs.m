fd989020a15c32083b813ab536eae35dcd4d9394
Minh Ngo <nlminhtl@gmail.com>
2016-03-28 12:22:52 +0200

    Adding result template generation.

--- final-project/train_SVMs.m
+++ final-project/train_SVMs.m
@@ -1,14 +1,18 @@
 % Support Vector Machine Training function
 
-function [svm1, svm2, svm3, svm4] = train_SVMs(target, data)
+function [svm1, svm2, svm3, svm4, tf] = train_SVMs(target, data)
     % Train the first SVM
-    svm1 = train_class(target, data, 1);
-    svm2 = train_class(target, data, 2);
-    svm3 = train_class(target, data, 3);
-    svm4 = train_class(target, data, 4);
+    [svm1, t1, f1] = train_class(target, data, 1);
+    [svm2, t2, f2] = train_class(target, data, 2);
+    [svm3, t3, f3] = train_class(target, data, 3);
+    [svm4, t4, f4] = train_class(target, data, 4);
+    
+    tf = [t1 f1; t2 f2; t3 f3; t4 f4];
 end
 
-function svm = train_class(target, data, class)
+function [svm, t, f] = train_class(target, data, class)
     target = reshape(double(target == class), size(target));
+    t = sum(target);
+    f = length(target) - t;
     svm = svmtrain(target, data, '-s 0 -t 2 -b 1 -q -c 3');
 end
