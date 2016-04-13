3d764466d354c320b3736a48c066db725a132064
Minh Ngo <nlminhtl@gmail.com>
2016-04-13 13:51:01 +0200

    Moar comments!

--- assignment-5/ICP.m
+++ assignment-5/ICP.m
@@ -18,8 +18,8 @@ function [rotation, translation, err] = ICP(source, target, niter, remove_outlie
         closest_points = zeros(1, size(source, 2));
         min_distances = zeros(1, size(source, 2));
 
-        % iterate through points in source cloud
-        %display('Computing distances');
+        % iterate through points in source cloud and find minimal distance
+        % to the target point cloud
         for x = 1:size(source, 2)
             distance2 = sum(bsxfun(@minus, target, source(:, x)).^2, 1);
             [min_distances(x), closest_points(x)] = min(distance2, [], 2);
@@ -49,8 +49,17 @@ function [rotation, translation, err] = ICP(source, target, niter, remove_outlie
 
         % Perform the SVD
         [U, ~, V] = svd(H);
+
+        % Bærentzen et al. (2012) "Guide to Computational Geometry
+        % Processing", Chapter 15, Table 15.1.
+        % det(U * V') should be +1.
         R = V * diag([1 1 det(U*V')]) * U';
 
+        % There is a bug in  Bærentzen et al. (2012) Chapter 15, Table 15.1.
+        % Rotation matrix is missing.
+        %
+        % Following the original paper Bels et al. (1993) "A Method for
+        % Registration of 3-D Shapes", formula (26) instead.
         t = pt_avg - R * ps_avg;
         source = bsxfun(@plus, R * source, t);
 
