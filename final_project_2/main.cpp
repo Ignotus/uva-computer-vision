a35f10d1b1abb96becd4ce9367ce30c2e19608f8
Minh Ngo <nlminhtl@gmail.com>
2016-05-18 14:16:42 +0200

    Reading frames

--- final_project_2/main.cpp
+++ final_project_2/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <boost/format.hpp>
+#include "Frame3D/Frame3D.h"
+
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello world" << std::endl;
-  return 0;
+    if (argc != 2)
+        return 0;
+
+    for (int i = 0; i < 8; ++i) {
+        Frame3D frame;
+        frame.load(boost::str((boost::format("%s/%05d.3df") % argv[1] % i)));
+    }
+
+    return 0;
 }
