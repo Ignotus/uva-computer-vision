ea440a8eaeee5e954c17755dca4255750dd4d542
Minh Ngo <nlminhtl@gmail.com>
2016-05-18 14:35:05 +0200

    Obtaining point clouds

--- final_project_2/main.cpp
+++ final_project_2/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <boost/format.hpp>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/eigen.hpp>
+    
 #include "Frame3D/Frame3D.h"
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr mat2IntegralPointCloud(const cv::Mat& depth_mat, const float focal_length, const float max_depth) {
+    assert(depth_mat.type() == CV_16U);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    const int half_width = depth_mat.cols / 2;
+    const int half_height = depth_mat.rows / 2;
+    const float inv_focal_length = 1.0 / focal_length;
+    point_cloud->points.reserve(depth_mat.rows * depth_mat.cols);
+    for (int y = 0; y < depth_mat.rows; y++) {
+        for (int x = 0; x < depth_mat.cols; x++) {
+            float z = depth_mat.at<ushort>(cv:: Point(x, y)) * 0.001;
+            if (z < max_depth && z > 0) {
+                point_cloud->points.emplace_back(static_cast<float>(x - half_width)  * z * inv_focal_length,
+                                                 static_cast<float>(y - half_height) * z * inv_focal_length,
+                                                 z);
+            } else {
+                point_cloud->points.emplace_back(x, y, NAN);
+            }
+        }
+    }
+    point_cloud->width = depth_mat.cols;
+    point_cloud->height = depth_mat.rows;
+    return point_cloud;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -10,6 +40,9 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < 8; ++i) {
         Frame3D frame;
         frame.load(boost::str((boost::format("%s/%05d.3df") % argv[1] % i)));
+        
+        pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud = mat2IntegralPointCloud(frame.depth_image_, frame.focal_length_, 0.5);
+        std::cout << "Points obtained " << point_cloud->size() << std::endl;
     }
 
     return 0;
