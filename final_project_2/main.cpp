b7a6393ca021e7935f77fca129d9941fe9cc89d5
Minh Ngo <nlminhtl@gmail.com>
2016-05-18 15:41:02 +0200

    Plotting normals

--- final_project_2/main.cpp
+++ final_project_2/main.cpp
@@ -65,6 +65,7 @@ int main(int argc, char *argv[]) {
         boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
         viewer->setBackgroundColor(0, 0, 0);
         viewer->addPointCloud<pcl::PointXYZ>(point_cloud, "sample cloud");
+        viewer->addPointCloudNormals<pcl::PointXYZ, pcl::PointNormal>(point_cloud, normals, 10, 0.05, "normals");
         viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
         viewer->addCoordinateSystem(1.0);
         viewer->initCameraParameters();
